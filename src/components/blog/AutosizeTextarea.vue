d2691d424062821302fa206794e680f28901c9cb
li-xiaojun <16167479@qq.com>
2018-04-10 14:02:30 +0800

    修复填充初始文字高度无法自适应的bug

---
+++ src/components/blog/AutosizeTextarea.vue
@@ -0,0 +1,43 @@
+<template>
+  <textarea @input="handleChange">{{ value }}</textarea>
+</template>
+
+<script>
+  import autosize from 'autosize'
+
+  export default {
+    name: "AutosizeTextArea",
+    props: ['value'],
+    data() {
+      return {}
+    },
+    mounted() {
+      autosize(this.$el)
+      // 更新一下内容，否则一上来的高度就固定了，如果文字很多，高度也不根据文字的高度变化
+      this.$nextTick(()=>{
+        autosize.update(this.$el)
+      })
+    },
+    methods: {
+      handleChange(e) {
+        this.$emit('textChange', e.target.value);
+      }
+    }
+
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "../../common/stylus/mixins.styl"
+  textarea
+    wh(100%, auto)
+    padding .4rem
+    font-size 1rem
+    border 1px solid #aeaeae
+    outline none
+    min-height 25rem
+    max-width 100%
+  textarea:focus
+    border 1px solid #409EFF
+
+</style>
