9959fe79fad8acf94179524b9e6b0c64e3491c00
li-xiaojun <16167479@qq.com>
2018-04-11 18:32:30 +0800

    add Setting Component.

---
+++ src/components/setting/QBSetting.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="setting-wrapper">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "QBSetting",
+    data() {
+      return {}
+    },
+    mounted() {
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "../../common/stylus/mixins.styl"
+  .setting-wrapper
+    fullwh()
+    overflow-y auto
+    padding:2rem
+
+</style>
