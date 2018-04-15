b0d152c07591247ae1774502450cfb8fc19268c2
li-xiaojun <16167479@qq.com>
2018-04-15 21:23:45 +0800

    let autotextarea support tab indent

--- src/components/blog/AutosizeTextarea.vue
+++ src/components/blog/AutosizeTextarea.vue
@@ -1,5 +1,5 @@
 <template>
-  <textarea @input="handleChange">{{ value }}</textarea>
+  <textarea @input="handleChange" @keydown.tab.prevent="addTabIndent()" ref="autoTextArea">{{ value }}</textarea>
 </template>
 
 <script>
@@ -14,13 +14,25 @@
     mounted() {
       autosize(this.$el)
       // 更新一下内容，否则一上来的高度就固定了，如果文字很多，高度也不根据文字的高度变化
-      this.$nextTick(()=>{
+      this.$nextTick(() => {
         autosize.update(this.$el)
       })
     },
     methods: {
       handleChange(e) {
         this.$emit('textChange', e.target.value);
+      },
+      addTabIndent() {
+        let start = this.$refs.autoTextArea.selectionStart;
+        let end = this.$refs.autoTextArea.selectionEnd;
+        // set textarea value to: text before caret + tab + text after caret
+        let newVal = this.value.substring(0, start)
+          + "\x20\x20\x20\x20" + this.value.substring(end);
+        this.$refs.autoTextArea.value = newVal;
+        this.$emit('textChange', newVal);
+
+        this.$refs.autoTextArea.setSelectionRange(start+4, end+4);
+
       }
     }
 
@@ -37,6 +49,7 @@
     outline none
     min-height 25rem
     max-width 100%
+
   textarea:focus
     border 1px solid #409EFF
 
